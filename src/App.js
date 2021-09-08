fbbd81ebe0f15f169eea3cbc0e1cf12c9fff9818
Camila Arruda <camilaam1802@gmail.com>
2021-09-08 19:09:46 -0300

    Adicionando elementos da parte superios da landing page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Oi</p>
+      <div className="main">
+        <div className="logo"></div>
+        <div className="center">
+        <div className="menu">
+          <div className="menu-right">
+            <h5>menu right</h5>
+          </div>
+          <div className="menu-left">
+          <h5>menu left</h5>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
